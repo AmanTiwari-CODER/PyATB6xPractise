ab25a172d1fc59bf02ac1367e829f3e9ef548d6e
Aman <amantiwari84044@gmail.com>
2025-10-11 08:24:48 +0530

    New Commit

---
+++ src/Learning/ex_03_Literals_Variables/Lab016_Literals.py
@@ -0,0 +1,5 @@
+age = 65
+age+1
+print(age)
+
+#answer will be 5 because we are not using assignment operator to assign the value.
---
+++ src/Learning/ex_03_Literals_Variables/Lab028_String_Double_Single_Diff.py
@@ -0,0 +1,11 @@
+c = 'C'
+c1 = "C"
+print(c)
+print(c)
+
+# dir = 'C:\pramod\n.txt'
+dir = r"C:\aman\n.txt" # raw - it will print as it is (ignore the escape seq.)
+print(dir)
+
+file_path = r"/Users/aman/PycharmProjects/PyATB6xLearning/src/ex_03_Literals_Variables/Lab028_String_Double_Single_Diff.py"
+print(file_path)
