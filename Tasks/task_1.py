3c52fdf9da903815ec0a405f51e7fa84c9addd14
Aman <amantiwari84044@gmail.com>
2025-10-18 16:12:25 +0530

    New files commited

---
+++ Tasks/task_1.py
@@ -0,0 +1,6 @@
+# # Fin the output of the below command.
+print("Pramod","Amit","John",123,sep="*",end=".")
+# #It will print
+# # Pramod*Amit*John*123.
+
+
--- src/Tasks/task_1.py
+++
@@ -1,4 +0,0 @@
-# Fin the output of the below command.
-print("Pramod","Amit","John",123,sep="*",end=".")
-#It will print
-# Pramod*Amit*John*123.
